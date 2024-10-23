dfe3a4fd03977d4b98ed100622a5e43481195754
bindu <bindu@Shiva_9686>
2024-10-23 12:31:38 +0530

    commit

---
+++ MVN/src/test/java/com/actiTime/testscript/CustomerModule.java
@@ -0,0 +1,13 @@
+package com.actiTime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+@Test
+	public void createCustomer()
+	{
+		Reporter.log("createCustomer", true);
+	}
+
+}
